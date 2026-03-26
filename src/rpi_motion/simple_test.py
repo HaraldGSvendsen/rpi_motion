6e7b3a9b7c92af0e69f53da0153621b14867b520
Harald <haraldgeorg+github@proton.me>
2026-03-26 22:43:26 +0100

    add video play test

--- src/rpi_motion/simple_test.py
+++ src/rpi_motion/simple_test.py
@@ -1,13 +1,39 @@
 from gpiozero import MotionSensor
 from signal import pause
+import vlc
+import time
+
+
+def play_video(filename):
+    # Create an instance
+    instance = vlc.Instance()
+
+    # Create a media player
+    player = instance.media_player_new(filename)
+
+    # Load the video file
+    media = instance.media_new()
+    player.set_media(media)
+
+    # Play the video
+    player.play()
+
+    # Wait for video to finish (optional)
+    while player.get_state() != vlc.State.Ended:
+        time.sleep(0.5)
+    print("Playback finished")
+
+
+
 
 pir = MotionSensor(18)
 
 def motion_function():
-    print("Motion Detected")
+    print("BEVEGELSE!")
+    play_video('/home/harald/test.mp4')
 
 def no_motion_function():
-    print("Motion stopped")
+    print("stopp")
 
 pir.when_motion = motion_function
 pir.when_no_motion = no_motion_function
